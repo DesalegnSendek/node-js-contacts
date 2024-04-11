74c218c2a7b0e4675b90fe96a1a476a22ea0c6f7
Desalegn Sendek Adugna <118896834+DesalegnSendek@users.noreply.github.com>
2024-04-11 21:25:31 +0300

    all requests

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
+//all contacts
 router.route("/").get((req, res) =>{
     // res.send("<h1>get all contacts</h1>");
     res.json({message: "get all contacts"});
 });
 
+//single contact
+router.route("/:id").get((req, res) =>{
+    // res.send("<h1>get all contacts</h1>");
+    res.status(200).json({message: `get contact for ${req.params.id}`});
+});
+//add contact to the list
+router.route("/").post((req, res) =>{
+    // res.send("<h1>get all contacts</h1>");
+    res.json({message: "create contacts"});
+});
 
+//update contact
+router.route("/:id").put((req, res) =>{
+    // res.send("<h1>get all contacts</h1>");
+    res.json({message: `update contact for ${req.params.id}`});
+});
+
+//delete single contact
+router.route("/:id").delete((req, res) =>{
+    // res.send("<h1>get all contacts</h1>");
+    res.json({message: `delete contact for ${req.params.id}`});
+});
 module.exports= router;
